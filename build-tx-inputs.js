fef8c39a76897314018da7f9ba1b64d31bf829f9
Vishal Menon <vishalmenon.92@gmail.com>
2018-10-13 22:16:14 +0530

    Update build-tx-inputs.js

--- build-tx-inputs.js
+++ build-tx-inputs.js
@@ -37,7 +37,7 @@ function build_TxInputs(addresses) {
     }
     console.log("BODY FROM UTXO REQUEST: ",body);
     if(body.status){
-     console.log("Request to/get_utxo returned body: ",body.message);
+     console.log("Request to/get_utxo returned body: ",body.message_array);
      let utxo_set=body.message;
 
      utxo_format(utxo_set)
