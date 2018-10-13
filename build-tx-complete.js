eeab8d1c324b041b36beff963bf40579c92a45f2
Vishal Menon <vishalmenon.92@gmail.com>
2018-10-13 18:55:44 +0530

    Update build-tx-complete.js

--- build-tx-complete.js
+++ build-tx-complete.js
@@ -21,7 +21,7 @@ const changeAddress='DBDAhnDHhs1qRdW2tURnc95JrAy5eK5WbW';
 const fee=2000;
 const digiurl = `http://localhost:${NI_PORT}/broadcastx`;
 const LPATH = process.env.DSIGN_LPATH;
-
+const NI_PORT = process.env.NI_PORT;
 //-o_o===SignTx==================================================|
 function sign_tx (inputs,outputs,fee,change,pk){
  return new Promise((resolve,reject)=>{
