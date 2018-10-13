fe90f119158f6eb1ea56a759fae585e658a353a0
Vishal Menon <vishalmenon.92@gmail.com>
2018-10-13 20:34:27 +0530

    Update build-tx-complete.js

--- build-tx-complete.js
+++ build-tx-complete.js
@@ -129,12 +129,12 @@ function broadcast_tx(outputs){
     .catch((err)=>{
      if(!err.message.status){
       let resp = errorSet.errorFunc("fail", JSON.stringify(err.message.message));
-      console.log("Rejecting: Caught error in broadcast_tx()", resp);
+      console.log("2.Rejecting: Caught error in broadcast_tx()", resp);
       reject(resp);
      }
      else if(!err.status){
       let resp = errorSet.errorFunc("fail",JSON.stringify(err.message));
-      console.log("Rejecting: Caught error in broadcast_tx()", resp);
+      console.log("2.Rejecting: Caught error in broadcast_tx()", resp);
       reject(resp);
      }
      else{
