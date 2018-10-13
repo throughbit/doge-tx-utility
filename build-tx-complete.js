a4b315f6fc25f12dd9f860a2b9cef29de4da5feb
Vishal Menon <vishalmenon.92@gmail.com>
2018-10-13 17:54:47 +0530

    Update build-tx-complete.js

--- build-tx-complete.js
+++ build-tx-complete.js
@@ -13,6 +13,7 @@ const utxo = require('./build-tx-inputs');
 const txS = require('./build-tx-outputs');
 const broadcaster = require('./Broadcaster');
 const holdup = require('./wait');
+var fs = require('fs');
 //-o_o===/modules================================================|
 //Obfuscate your pk even further than an env variable.
 //However, since this module will be isolated form the internet, env should suffice
@@ -20,6 +21,9 @@ const WIF = process.env.WIF;
 const imported = digibyte.PrivateKey.fromWIF(WIF).toString();
 const changeAddress='DBDAhnDHhs1qRdW2tURnc95JrAy5eK5WbW';
 const fee=2000;
+const digiurl = `http://localhost:${NI_PORT}/broadcastx`;
+const LPATH = process.env.DSIGN_LPATH;
+
 //-o_o===SignTx==================================================|
 function sign_tx (inputs,outputs,fee,change,pk){
  return new Promise((resolve,reject)=>{
@@ -38,6 +42,20 @@ function sign_tx (inputs,outputs,fee,change,pk){
      transaction.fee(fee)
      .change(change)
      .sign(pk);
+    
+     let log_data={
+      "time":Date.getTime(),
+      "status": "success",
+      "message": Transactions have been signed. 
+      "transaction_outputs": outputs
+      "hex":transaction
+     }
+     let log_separator = "#--------------------------------------------------------------------------------------------------------------- ";
+     
+     fs.appendFile(`${LPATH}`,`${JSON.stringify(log_data,null,1)}\n${log_separator}\n`, function(err){
+      if(err) console.log("Could not write to file: \n", err);
+      else console.log(`Notification logged @ ${log_data.time}. Check ${LPATH}.`);
+     });
      resolve (transaction);
     }
    }
@@ -45,6 +63,22 @@ function sign_tx (inputs,outputs,fee,change,pk){
   }
   catch(e){
    console.log("Rejecting: Caught error at sign_tx()",e);
+   
+   let log_data={
+    "time":Date.getTime(),
+    "status": "fail",
+    "message":`Error signing transactions.\nError: ${e}`,
+    "transaction_outputs": outputs
+    //next level: split addresses from amounts.
+    //see if this runs for now
+   }
+   let log_separator = "#--------------------------------------------------------------------------------------------------------------- ";
+
+   fs.appendFile(`${LPATH}`,`${JSON.stringify(log_data,null,1)}\n${log_separator}\n`, function(err){
+    if(err) console.log("Could not write to file: \n", err);
+    else console.log(`Notification logged @ ${log_data.time}. Check ${LPATH}.`);
+   });
+   
    reject (e);
   }
  });
