d131d34dbc128e4ad6c6230af7f26ee31c68415b
Vishal Menon <vishalmenon.92@gmail.com>
2018-10-14 00:51:34 +0530

    Update logs.js

--- logs.js
+++ logs.js
@@ -2,7 +2,7 @@ use strict;
 //-o_O===modules===================================================~|
 const fs = require('fs');
 //-o_O===init======================================================~|
-
+//-o_O===sign_tx()=================================================~|
 function write_sign_log(status,data){
  const LPATH = process.env.DSIGN_LPATH;
 //status is a bool
@@ -10,13 +10,13 @@ function write_sign_log(status,data){
  const time = new Date().getTime();
 
  var s_log={
-      "time":time,
-      "status": "success",
-      "message": "Transactions have been signed.",
-      "transaction_outputs": data.outputs,
-      "transaction_inputs": data.inputs,
-      "hex":data.hex
-     }
+  "time":time,
+  "status": "success",
+  "message": "Transactions have been signed.",
+  "transaction_outputs": data.outputs,
+  "transaction_inputs": data.inputs,
+  "hex":data.hex
+ }
 
  var f_log={
   "time":time,
@@ -29,19 +29,18 @@ function write_sign_log(status,data){
 }
  var log_separator = "#--------------------------------------------------------------------------------------------------------------- ";
  if(status){
-   fs.appendFile(`${LPATH}`,`${JSON.stringify(s_log,null,1)}\n${log_separator}\n`, function(err){
-    if(err) console.log("Could not write to file: \n", err);
-    else console.log(`Notification logged @ ${time}. Check ${LPATH}.`);
-   });
- }
- else{
-   fs.appendFile(`${LPATH}`,`${JSON.stringify(f_log,null,1)}\n${log_separator}\n`, function(err){
-    if(err) console.log("Could not write to file: \n" , err);
-    else console.log(`Notification logged @ ${time}. Check ${LPATH}.`);
+ fs.appendFile(`${LPATH}`,`${JSON.stringify(s_log,null,1)}\n${log_separator}\n`, function(err){
+   if(err) console.log("Could not write to file: \n", err);
+   else console.log(`Notification logged @ ${time}. Check ${LPATH}.`);
   });
  }
+ else{
+  fs.appendFile(`${LPATH}`,`${JSON.stringify(f_log,null,1)}\n${log_separator}\n`, function(err){
+   if(err) console.log("Could not write to file: \n" , err);
+   else console.log(`Notification logged @ ${time}. Check ${LPATH}.`);
+ });
+ }
 }
-
 //-o_O===exports===================================================~|
 module.exports={write_sign_log};
 //-o_O===fin-======================================================~|
