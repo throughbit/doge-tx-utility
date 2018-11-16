1cab5e7f7d26c36f29d83cbcfd646cbc741a7f94
Digibyte <vishalmenon.92@gmail.com>
2018-11-16 14:10:25 +0530

    0.1.0. Improved database management

--- listener.js
+++ listener.js
@@ -49,91 +49,97 @@ app.post('/test_send',(req,res)=>{
     //   errors.handle(e);
     // })
     //-------------------------------------------LOGIC------------------------------------------------------
-
     let send_orders = [
     {
       "address": "DBDAhnDHhs1qRdW2tURnc95JrAy5eK5WbW",
       "amount":5000,
-      "orderId":"TBDGB-9J298IJQ1234"
+      "orderId":"TBDGB-9J298IJQ1238"
     },
     {
       "address": "DBDAhnDHhs1qRdW2tURnc95JrAy5eK5WbW",
       "amount":5000,
-      "orderId":"TBDGB-95T698IJQ1235"
+      "orderId":"TBDGB-95T698IJQ123769"
     },
     {
       "address": "DBDAhnDHhs1qRdW2tURnc95JrAy5eK5WbW",
       "amount":5000,
-      "orderId":"TBDGB-9KL98IJQ1236"
+      "orderId":"TBDGB-9KL98IJQ12362"
     }
     ];
     
-    let filtered_orders=new Array();
-    let output_set=new Array();
-    let report_txid=new Array();
-    // filter_request(send_orders)
-    // .then((outputs)=>{
-    //   console.log(outputs);
-    //   res.send(JSON.stringify(outputs));
-    // })
-    // .catch((e)=>{
-    //   console.log(e);
-    // });
-// console.log("OUtput set length", output_set.length);  
+    let filtered_orders=new Array();//includes orderId
+    let output_set=new Array();//output_set passed for signing
+    let report_txid=new Array();//completed orders with txid for reporting back to client
 //-o_o===filter-request--==========================================|
-    async_looper.go(send_orders,(orders,report)=>{ //orders is the iterator 'send_orders[i]' in the for loop defined in async_loop
-      transactionSchema.findOne({orderId:orders.orderId})//search db for orders with this orderId
+    async_looper.go(send_orders,(order,report)=>{ //order is the iterator 'send_orders[i]' in the for loop defined in async_loop
+      transactionSchema.findOne({orderId:order.orderId})//search db for order with this orderId
       .exec((err,data)=>{
         
         let output = {
           "address": "",
           "amount": 0
         };
-
+//if order is not in db, add it and push it into output_set for sign and broadcast
         if(data===null){
-          console.log(`Okay, zis Order:${orders.orderId} is not exist. Don't a worry. I will a send for process!`);
-          
-          filtered_orders.push(orders);
+          console.log(`Okay, zis Order:${order.orderId} is not exist. Don't a worry. I will a send for process!`);
+        
+          let _tx = new transactionSchema({
+            orderId : order.orderId,//can be added in the previous mongo call. 
+            to: order.address,
+            amount:order.amount
+          });
 
-          output.address=orders.address;
-          output.amount=orders.amount;
+          _tx.save((e)=> { 
+            if(e!=null){
+              console.log("Error writing to dB.");
+              res.send(errors.handle(e));
+            }
+            else{
+              console.log(`Added Order:${order.orderId} to dB.`);
+            }
+          });
+        
+          filtered_orders.push(order);
+
+          output.address=order.address;
+          output.amount=order.amount;
           output_set.push(output);
           
           report();
         }
+//if order is in db check if it has a txid or not
         else if(data!=null){
         // console.log("Found entry with matching orderId",data);
+// if it has a txid, push it into report_txid to notify client
           if (data.txId){
-            console.log(`Order: ${data.orderId} has already been processed.\ntxId: ${data.txId} `);
+            console.log(`Order: ${data.orderId} has already been processed.\ntxId: ${data.txId}`);
             report_txid.push({
               "orderId": data.orderId,
-              "txid": data.txId});
+              "txId": data.txId});
             report();
           }
-          else {
-            console.log(`No txid for:${orders.orderId}. Added for processzing.\n`,data);
-           
-            filtered_orders.push(orders);
+// if it doesnt have a txid, push it into output_set to sign and broadcast
+          else{
+            console.log(`No txId for ${data.orderId}.  Added for processzing. \n`);
+            
+            filtered_orders.push(order);
 
-            output.address=orders.address;
-            output.amount=orders.amount;
+            output.address=order.address;
+            output.amount=order.amount;
             output_set.push(output);
           
             report();
           }
         }
-          else if(err!=null){
-            console.log("Demn. V r getsing an error!", err);
-            res.send(errors.handle(err));
-            //we cannot report here since this is an run-time error
-          }
-      });
-    },()=>{//this is called only once all 
-      //  console.log("OUTPUTS",output_set);
-      //  console.log("OUtput set length", output_set.length);  
-      if(report_txid.length!=0){//if there are completed orderId's with txId, report those.
 
-      }
+        else if(err!=null){
+          console.log("Demn. V r getsing an error!", err);
+          res.send(errors.handle(err));
+          //we cannot report here since this is an run-time error
+        }
+      });
+    },()=>{//this is called only once all send_orders are filtered are reported
+
       // console.log(filtered_orders);
       if(output_set.length===0){
         console.log("OutputSet is empty. All transactions have been processed.");
@@ -149,6 +155,11 @@ app.post('/test_send',(req,res)=>{
       if(output_set.length>0){
         process_and_update(output_set,filtered_orders)
         .then((response)=>{//signed,broadcasted and updated to db
+          if(report_txid.length!=0){//if some transactions were already done, add it to the response set
+           report_txid.forEach((element)=>{
+             response.message.push(element);
+           })  
+          }
           console.log("FINAL RESPONSE:\n", response);
           res.send(response);
         })
@@ -181,74 +192,49 @@ let process_and_update=(output_set,filtered_orders)=>{
       let order_txid=new Array();
       tx_util.build(output_set)
       .then((tx_hex)=>{//is signed
+        tx_util.broadcast(tx_hex.message.toString())
+        .then((txid)=>{
+          async_looper.go(filtered_orders,(order,report)=>{
+            transactionSchema.findOne({orderId: order.orderId})
+            .exec((err,data)=>{
+              if(err!=null){
+                //If no error was thrown when creating db entry at tx_util.build() whats up here? 
+                //Control should not ever be reaching here in production!
+                console.log("Error writing to dB. Uh-oh PANIC!!This transaction could be broadcasted twice! Notify client!")
+              }
 
-        async_looper.go(filtered_orders,(element,report)=>{
-          transactionSchema.findOne({orderId:element.orderId})
-          .exec((err,data)=>{
-            if(data===null){
-              let _tx = new transactionSchema({
-                orderId : element.orderId,//can be added in the previous mongo call. 
-                to: element.address,
-                amount:element.amount
+              let dataId = data._id;
+              delete data._id;
+      
+              data.txId = txid.message;
+              
+              order_txid.push({
+                "orderId":order.orderId,
+                "txId":txid.message
               });
-              _tx.save((e)=> { 
-                if(e!=null){
-                  console.log("Error writing to dB. Transaction will not be broadcasted.");
-                  reject(errors.handle(e));
-                }
-                report();
-              });
-            }
-            else if(err!=null){
-              reject(errors.handle(err));
-            }
-            report();
-          });
-        },()=>{//broadcast will only be called if write to db is successful
-          tx_util.broadcast(tx_hex.message.toString())
-          .then((txid)=>{
-            async_looper.go(filtered_orders,(orders,report)=>{
-              transactionSchema.findOne({orderId: orders.orderId})
-              .exec((err,data)=>{
+
+              transactionSchema.updateOne({ _id: {$eq:ObjectId(dataId)} }, data, (err,data)=>{
                 if(err!=null){
                   //If no error was thrown when creating db entry at tx_util.build() whats up here? 
                   //Control should not ever be reaching here in production!
-                  console.log("Error writing to dB. Uh-oh PANIC!!This transaction could be broadcasted twice! Notify client!")
+                  console.log("Error writing to dB. Uh-oh PANIC!! This transaction could be broadcasted twice! Notify orderbook that txid is issued and db is not updated.")
                 }
-
-                let dataId = data._id;
-                delete data._id;
-        
-                data.txId = txid.message;
+                if (data!=null){
+                  console.log("Successfully updated db");
                 
-                order_txid.push({
-                  "orderId":orders.orderId,
-                  "txId":txid.message
-                });
-
-                transactionSchema.updateOne({ _id: {$eq:ObjectId(dataId)} }, data, (err,data)=>{
-                  if(err!=null){
-                    //If no error was thrown when creating db entry at tx_util.build() whats up here? 
-                    //Control should not ever be reaching here in production!
-                    console.log("Error writing to dB. Uh-oh PANIC!! This transaction could be broadcasted twice! Notify orderbook that txid is issued and db is not updated.")
-                  }
-                  if (data!=null){
-                    console.log("Successfully updated db");
-                  
-                    report();
-                  }
-                });
+                  report();
+                }
               });
-            },()=>{
-                let response = res_fmt.create(true, order_txid);
-                console.log(`Success Response: toFinal->`, response);
-                console.log("\nHERE!!!!!!!!!!!!\n");
-                resolve(response);
-            });     
-          })
-          .catch((e)=>{
-            reject(e);
-          })
+            });
+          },()=>{
+              let response = res_fmt.create(true, order_txid);
+              console.log(`Success Response: toFinal->`, response);
+              console.log("\nHERE!!!!!!!!!!!!\n");
+              resolve(response);
+          });     
+        })
+        .catch((e)=>{
+          reject(e);
         });
       })
       .catch((e)=>{
